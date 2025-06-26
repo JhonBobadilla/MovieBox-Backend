ac7d8b96831127e0b2f916f31619a59c46c416cb
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2025-06-26 15:21:28 -0500

    chore: crear estructura base de carpetas para el proyecto

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API MovieBox-Backend funcionando');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
